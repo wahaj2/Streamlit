46a9e7bc1d908ec3eeb86ecd5b9bfc7b7f058670
wahaj2 <wahajk901@gmail.com>
2025-03-22 15:46:57 +0500

    Initial commit

--- app.py
+++ app.py
@@ -2,26 +2,56 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import pandas as pd
+import matplotlib.pyplot as plt
 
-# Title of the app
-st.title("My First Streamlit App")
 
-# User input
-st.write("## Enter a Number")
-number = st.slider("Select a number", 1, 100, 50)
 
-# Display the selected number
-st.write(f"You selected: **{number}**")
+tables = pd.read_html("https://www.feanalytics.com/Table.aspx?SavedResultsID=e33df1a2-7b9a-ee11-b204-002248818b97&xl=1&UserID=B2E540A8-D474-4C56-9088-1F155612F7FE&xlRefreshData=1")
+#Saving the data into Dateframe
+data = tables[0]
+#Taking the transpose of table
+data = data.T
+#Spliting the dates from string format 
+data[1][1][26:36]
+#Looping into column and saving the dates in list 
+dates = []
+for i in range (1,71):
+    date = data[1][i][26:36]
+    dates.append(date)
+#Droping the first and second columns
+data = data.drop(columns=[0])
+data = data.drop(columns=[1])
+#Naming the columns 
+data.columns = data.iloc[0]
+data = data.iloc[1:].reset_index(drop=True)
+#Now droping the Null values rows 
+data.dropna(inplace = True)
+#Adding the dates 
+data['Dates'] = dates
+#Converting the date into pandas datetime format
+data['Dates'] = pd.to_datetime(data.Dates)
+#Setting the Dates as index in our table
+data.set_index('Dates', inplace= True)
+#Changing the column data into float values
+for i in range(len(data.columns)):
+    data[data.columns[i]]=data[data.columns[i]].astype(float)
 
-# Create a line chart
-st.write("## Line Chart")
-data = pd.DataFrame({
-    "x": range(number),
-    "y": [i ** 2 for i in range(number)]
-})
-fig = px.line(data, x="x", y="y", title="y = x^2")
-st.plotly_chart(fig)
 
-# Add some text
-st.write("## About This App")
-st.write("This is a simple Streamlit app hosted on Streamlit Sharing. It demonstrates how to create interactive web apps using Python!")
+
+sectors = data.columns.tolist()
+
+# Streamlit app
+st.title('Sector Performance Dashboard')
+
+# Dropdown to select a sector
+selected_sector = st.selectbox('Select a Sector', sectors)
+
+# Plot the selected sector's data
+st.write(f"### {selected_sector} Performance Over Time")
+fig, ax = plt.subplots()
+ax.plot(data.index, data[selected_sector], label=selected_sector)
+ax.set_xlabel('Date')
+ax.set_ylabel('Performance')
+ax.legend()
+st.pyplot(fig)
